a39ba5763cdc1057af99be7687f6078f2de7a8cf
相原大介 <aiharadaisuke@aiharadaisukenoMacBook-Air.local>
2020-12-15 12:07:01 +0900

    ７回目のgithub

---
+++ js/maintest4.js
@@ -0,0 +1,17 @@
+/* global $*/
+$(function() {
+    $('.slider').slick( {
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        dotsClass:'slide-dots'
+    });
+});
+
+alert('ok');
